129596c4f76967ab7139ceac792a138cf18b3d66
andresiturria <andresiturria96@gmail.com>
2022-11-06 13:09:26 +0100

    bug fix and changed to binary focal loss

--- andres/launch.py
+++ andres/launch.py
@@ -2,9 +2,7 @@ import os
 import json
 import numpy as np
 import math
-
-from matplotlib import cm
-
+from confusion_matrix import draw_confusion_matrix
 from generic_objects import GenericImage, GenericObject
 from generators import generator_images, load_geoimage
 from keras.models import Sequential
@@ -12,17 +10,17 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.optimizers import adam_v2
 from keras.callbacks import TerminateOnNaN, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from sklearn.model_selection import train_test_split
-from focal_loss import SparseCategoricalFocalLoss
-
+from sklearn.metrics import confusion_matrix
+from focal_loss import BinaryFocalLoss
 
 
 # Hyper-parameters
 batch_size = 16
 epochs = 20
-opt = adam_v2.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.00, amsgrad=True, clipnorm=1.0, clipvalue=0.5)
-loss_funct = 'categorical_crossentropy'  # I hardcoded this as its not from keras
-number_of_hidden_layers = 0
-neurons_per_hidden_layer = [100]
+opt = adam_v2.Adam(learning_rate=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.00, amsgrad=True, clipnorm=1.0, clipvalue=0.5)
+#loss_funct = 'categorical_crossentropy'   I hardcoded this as BinaryFocalLoss is not from keras
+number_of_hidden_layers = 1
+neurons_per_hidden_layer = [200]
 activation = 'relu'
 dropout = 0.2
 
@@ -30,8 +28,10 @@ dropout = 0.2
 categories = {13: 'CARGO_PLANE', 15: 'HELICOPTER', 18: 'SMALL_CAR', 19: 'BUS', 23: 'TRUCK', 41: 'MOTORBOAT',
               47: 'FISHING_VESSEL', 60: 'DUMP_TRUCK', 64: 'EXCAVATOR', 73: 'BUILDING', 86: 'STORAGE_TANK',
               91: 'SHIPPING_CONTAINER'}
+image_cap = 5000
 
-dataset_dirpath = '../../PROJECT/xview_recognition'
+# dataset_dirpath = '../../PROJECT/xview_recognition'
+dataset_dirpath = 'xview_recognition'
 
 # Load database
 json_file = os.path.join(dataset_dirpath, 'xview_ann_train.json')
@@ -49,7 +49,7 @@ for json_img, json_ann in zip(json_data['images'], json_data['annotations']):
     obj.bb = (int(json_ann['bbox'][0]), int(json_ann['bbox'][1]), int(json_ann['bbox'][2]), int(json_ann['bbox'][3]))
     obj.category = list(categories.values())[json_ann['category_id']-1]
     # Resampling strategy to reduce training time
-    if counts[obj.category] >= 5000:
+    if counts[obj.category] >= image_cap:
         continue
     counts[obj.category] += 1
     image.add_object(obj)
@@ -63,7 +63,7 @@ anns_train, anns_valid = train_test_split(anns, test_size=0.1, random_state=1, s
 model = Sequential()
 model.add(Flatten(input_shape=(224, 224, 3)))
 model.add(Activation(activation))
-
+model.add(Dropout(dropout))
 for i in range(number_of_hidden_layers):
     model.add(Dense(neurons_per_hidden_layer[i]))
     model.add(Activation(activation))
@@ -72,7 +72,7 @@ for i in range(number_of_hidden_layers):
 model.add(Dense(len(categories)))
 model.add(Activation('softmax'))
 model.summary()
-model.compile(optimizer=opt, loss=loss_funct, metrics=['accuracy'])
+model.compile(optimizer=opt, loss=BinaryFocalLoss(gamma=2), metrics=['accuracy'])
 
 # Callbacks
 model_checkpoint = ModelCheckpoint('model.hdf5', monitor='val_accuracy', verbose=1, save_best_only=True)
@@ -95,7 +95,8 @@ valid_generator = generator_images(categories, objs_valid, batch_size, do_shuffl
 
 train_steps = math.ceil(len(objs_train)/batch_size)
 valid_steps = math.ceil(len(objs_valid)/batch_size)
-h = model.fit(train_generator, steps_per_epoch=train_steps, validation_data=valid_generator, validation_steps=valid_steps, epochs=epochs, callbacks=callbacks, verbose=1)
+h = model.fit(train_generator, steps_per_epoch=train_steps, validation_data=valid_generator,
+              validation_steps=valid_steps, epochs=epochs, callbacks=callbacks, verbose=1)
 # Best validation model
 best_idx = int(np.argmax(h.history['val_accuracy']))
 best_value = np.max(h.history['val_accuracy'])
@@ -110,8 +111,9 @@ with open(json_file) as ifs:
     json_data = json.load(ifs)
 ifs.close()
 anns = []
+
 for json_img, json_ann in zip(json_data['images'], json_data['annotations']):
-    image = GenericImage(dataset_dirpath + json_img['file_name'])
+    image = GenericImage(os.path.join(dataset_dirpath, json_img['file_name']))
     image.tile = np.array([0, 0, json_img['width'], json_img['height']])
     obj = GenericObject()
     obj.id = json_ann['id']
@@ -119,6 +121,7 @@ for json_img, json_ann in zip(json_data['images'], json_data['annotations']):
     obj.category = list(categories.values())[json_ann['category_id']-1]
     image.add_object(obj)
     anns.append(image)
+
 
 y_true, y_pred = [], []
 for ann in anns:
@@ -133,6 +136,9 @@ for ann in anns:
         pred_score = np.max(predictions)
         y_true.append(obj_pred.category)
         y_pred.append(pred_category)
+
+cm = confusion_matrix(y_true, y_pred, labels=list(categories.values()))
+draw_confusion_matrix(cm, categories, normalize=True)
 
 correct_samples_class = np.diag(cm).astype(float)
 total_samples_class = np.sum(cm, axis=1).astype(float)
